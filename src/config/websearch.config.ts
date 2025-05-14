21dac9dfc55b5de884cdf48961babd704d1f5390
Tomasz Bernat <mg42sd@gmail.com>
2025-05-14 12:41:24 +0200

    Initial commit with skeleton project.

---
+++ src/config/websearch.config.ts
@@ -0,0 +1,22 @@
+export const whitelistedDomains = [
+    { name: 'wikipedia', url: 'wikipedia.org', scrappable: true },
+    { name: 'arxiv', url: 'arxiv.org', scrappable: true },
+    { name: 'instagram', url: 'instagram.com', scrappable: false },
+    { name: 'openai', url: 'openai.com', scrappable: true },
+    { name: 'reuters', url: 'reuters.com', scrappable: true },
+    { name: 'mit technology review', url: 'technologyreview.com', scrappable: true },
+    { name: 'youtube', url: 'youtube.com', scrappable: false },
+    { name: 'tailwind css', url: 'tailwindcss.com', scrappable: true },
+    { name: 'imdb', url: 'imdb.com', scrappable: true },
+    { name: 'techcrunch', url: 'techcrunch.com', scrappable: true },
+    { name: 'hacker news', url: 'news.ycombinator.com', scrappable: true },
+    { name: 'anthropic', url: 'anthropic.com', scrappable: true },
+    { name: 'DeepMind', url: 'deepmind.google', scrappable: true },
+    { name: 'musicbrainz', url: 'musicbrainz.org', scrappable: true },
+    { name: 'sonos', url: 'sonos.com', scrappable: true },
+    { name: 'github', url: 'github.com', scrappable: true },
+    { name: 'stack overflow', url: 'stackoverflow.com', scrappable: true },
+    { name: 'medium', url: 'medium.com', scrappable: true },
+    { name: 'dev.to', url: 'dev.to', scrappable: true },
+    { name: 'reddit', url: 'reddit.com', scrappable: true }
+];
